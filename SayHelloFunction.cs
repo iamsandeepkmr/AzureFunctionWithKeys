a36ba7ca9b4e392c321366640771f3263232a88e
Sandeep Kumar <sandeep.kumar@kongsbergdigital.com>
2022-12-02 13:21:49 +0530

    Checking in source code

---
+++ SayHelloFunction.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace GreetAzFunction
+{
+    public static class SayHello
+    {
+        [FunctionName("SayHello")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult("Hello there!");
+        }
+    }
+}
+
